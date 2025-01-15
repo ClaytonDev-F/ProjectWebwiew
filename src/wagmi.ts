4dcab922023028e9a2e1693488a8f545391df8f5
ClaytonDev-F <fernandaflauzino1333@gmail.com>
2025-01-15 11:36:35 -0300

    First Commit

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,23 +1,14 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
-  sepolia,
+  bsc,
 } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  projectId: '50fdf83633b3224814645f65c0656aff',
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+   bsc,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [bsc] : []),
   ],
   ssr: true,
 });
